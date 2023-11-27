1b0351d5ac164e4979754925343f4e936aabe90d
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2023-11-26 22:08:22 -0600

    issue/14: Add license header and copyright note to all source files

--- skippy-core/src/test/java/io/skippy/core/TestImpactAnalysisTest.java
+++ skippy-core/src/test/java/io/skippy/core/TestImpactAnalysisTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.skippy.core;
 
 import org.junit.jupiter.api.Test;
@@ -8,6 +24,11 @@ import java.nio.file.Path;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Tests for {@link TestImpactAnalysis}.
+ *
+ * @author Florian McKee
+ */
 public class TestImpactAnalysisTest {
 
     @Test
