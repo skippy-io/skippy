c3a4356c39251ec2b5db2bdd9e1900454a1b303e
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2023-12-23 10:56:03 -0600

    issues/63: Improve test coverage for skippy-junit4 & skippy-junit5 (#70)

---
+++ skippy-junit4/src/test/java/io/skippy/junit4/SkipOrExecuteRuleTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.skippy.junit4;
+
+import io.skippy.core.SkippyAnalysis;
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests for {@link SkipOrExecuteRule}
+ */
+public class SkipOrExecuteRuleTest {
+
+    SkippyAnalysis skippyAnalysis = mock(SkippyAnalysis.class);
+    Statement base = mock(Statement.class);
+    Description description = mock(Description.class);
+
+    @Test
+    public void testExecutionofTest() throws Throwable {
+        doReturn(SkipOrExecuteRuleTest.class).when(description).getTestClass();
+        var rule = new SkipOrExecuteRule(skippyAnalysis);
+        when(skippyAnalysis.testNeedsToBeExecuted(SkipOrExecuteRuleTest.class)).thenReturn(true);
+        rule.apply(base, description).evaluate();
+        verify(base).evaluate();
+    }
+
+    @Test
+    public void testSkippingOfTest() throws Throwable {
+        doReturn(SkipOrExecuteRuleTest.class).when(description).getTestClass();
+        var rule = new SkipOrExecuteRule(skippyAnalysis);
+        when(skippyAnalysis.testNeedsToBeExecuted(SkipOrExecuteRuleTest.class)).thenReturn(false);
+        rule.apply(base, description).evaluate();
+        verify(base, times(0)).evaluate();
+    }
+
+}
