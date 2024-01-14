c34628a053865ec4541990afa6e8f869f1de9ab7
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2024-01-14 12:34:02 -0600

    issues/127: Resolve JavaDoc warnings during build (#130)

--- skippy-common/src/main/java/io/skippy/core/SkippyUtils.java
+++ skippy-common/src/main/java/io/skippy/core/SkippyUtils.java
@@ -1,28 +1,73 @@
+/*
+ * Copyright 2023-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.skippy.core;
 
 import java.nio.file.Path;
 
 import static io.skippy.core.SkippyConstants.SKIPPY_DIRECTORY;
 
-public class SkippyUtils {
+/**
+ * A couple of static methods for retrieval and creation of the skippy folder.
+ *
+ * @author Florian McKee
+ */
+public final class SkippyUtils {
 
+    /**
+     * Returns the skippy folder. This method will create the folder if it doesn't exist.
+     *
+     * @return the skippy folder
+     */
     public static Path getOrCreateSkippyFolder() {
         return getOrCreateSkippyFolder(Path.of("."));
     }
 
-    public static Path getOrCreateSkippyFolder(Path projectDir) {
-        var skippyFolder = getSkippyFolder(projectDir);
+    /**
+     * Returns the skippy folder in the given {@code projectFolder}. This method will create the folder if it doesn't
+     * exist.
+     *
+     * @param projectFolder the project's root folder
+     * @return the skippy folder in the given {@code projectFolder}
+     */
+    public static Path getOrCreateSkippyFolder(Path projectFolder) {
+        var skippyFolder = getSkippyFolder(projectFolder);
         if ( ! skippyFolder.toFile().exists()) {
             skippyFolder.toFile().mkdirs();
         }
         return skippyFolder;
     }
 
-    public static Path getSkippyFolder(Path projectDir) {
-        return projectDir.resolve(SKIPPY_DIRECTORY);
+    /**
+     * Returns the skippy folder in the given {@code projectFolder}.
+     *
+     * @param projectFolder the project's root folder
+     * @return the skippy folder in the given {@code projectFolder}
+     */
+    public static Path getSkippyFolder(Path projectFolder) {
+        return projectFolder.resolve(SKIPPY_DIRECTORY);
     }
 
+    /**
+     * Returns the skippy folder.
+     *
+     * @return the skippy folder
+     */
     public static Path getSkippyFolder() {
-        return SKIPPY_DIRECTORY;
+        return getSkippyFolder(Path.of("."));
     }
+
 }
