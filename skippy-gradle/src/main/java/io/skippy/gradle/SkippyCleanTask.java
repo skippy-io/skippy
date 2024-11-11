6bc81f597af3c00509402e6b9e0daf48baf8ed50
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2024-11-11 09:58:45 -0600

    Issues/186: skippyClean: add support for --rerun option (#188)

--- skippy-gradle/src/main/java/io/skippy/gradle/SkippyCleanTask.java
+++ skippy-gradle/src/main/java/io/skippy/gradle/SkippyCleanTask.java
@@ -19,7 +19,7 @@ package io.skippy.gradle;
 import io.skippy.core.SkippyBuildApi;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.provider.Property;
-import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.Internal;
 
 import javax.inject.Inject;
 
@@ -32,7 +32,7 @@ import javax.inject.Inject;
  */
 abstract class SkippyCleanTask extends DefaultTask {
 
-    @Input
+    @Internal
     abstract Property<ProjectSettings> getProjectSettings();
 
     @Inject
